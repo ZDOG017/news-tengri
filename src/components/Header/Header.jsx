15d4060dae8e8c683872efdb2d603c62fb3d525a
Soltan <iamkazakh02@gmail.com>
2024-04-16 21:46:48 +0600

    first touches

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import { formatDate } from "../../helpers/formatDate";
+import styles from "./styles.module.css";
 const Header = () => {
-    return (
-    <header>
-        <h1>NEWS TENGRI</h1>
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>NEWS TENGRI</h1>
+      <p className={styles.date}>{formatDate(new Date())}</p>
     </header>
-    );
+);
 };
 
-export default Header
+export default Header;
